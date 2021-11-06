7c5a071cfe0dbbe2ccf4f08960577752ee58541f
sdudiak <staszek.dudiak@irpark.pl>
2021-11-06 17:09:50 +0100

    first draft of Server classes, waitig for Product class

--- servers.py
+++ servers.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from typing import Optional
+# from _typeshed import Self
+from typing import List, Optional
+from abc import ABC, abstractmethod
 
 
 class Product:
@@ -27,12 +29,57 @@ class TooManyProductsFoundError:
 #  dopuszczalną liczbę wyników wyszukiwania, (3) możliwość odwołania się do metody `get_entries(self, n_letters)`
 #  zwracającą listę produktów spełniających kryterium wyszukiwania
 
-class ListServer:
-    pass
+class Server(ABC):
+    def __init__(self) -> None:
+        self.products = 0
+        super().__init__()
+    n_max_returned_entries = 10
+    
+    def get_entries(self,n_letters:int = 1) -> List[Product]:
+        products = self.get_all_products(self.products)
+        product_list = []
+        ascii_letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        digits = '0123456789'
+        for product in products:
+            n_chars = product.name[0:n_letters]
+            nums2 = product.name[n_letters:n_letters + 2]
+            nums3 = product.name[n_letters:n_letters + 3]
+            if all(item in ascii_letters for item in n_chars):
+                if all(item in digits for item in nums2) or all(item in digits for item in nums3):
+                    product_list.append(product)
+            if len(product_list) > self.n_max_returned_entries:
+                print('too many entries')
+                return 0 
+            product_list = sorted(product_list,key = product.price)
+        return product_list
+
+    @abstractmethod
+    def get_all_products(self):
+        pass
 
 
-class MapServer:
-    pass
+class ListServer(Server):
+    def __init__(self,products) -> None:
+        super().__init__(self)
+
+        self.products = products
+
+    def get_all_products(self):
+        return self.products
+
+class MapServer(Server):
+
+    def __init__(self,products) -> None:
+        super().__init__(self)
+        self.products = {}
+        for product in products:
+            self.products[product.name] = product
+
+    def get_all_products(self):
+        product_list = []
+        for product in self.products:
+            product_list.append(product)
+        return product_list
 
 
 class Client:
